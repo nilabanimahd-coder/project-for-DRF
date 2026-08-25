e9879463fd7323b15775adf1b2cf576e256c5263
nila <nilabmd2@gmail.com>
2026-08-25 17:43:39 +0330

    add put,patch,delete function

--- book/views.py
+++ book/views.py
@@ -45,3 +45,24 @@ class BookDetailView(APIView):
         ser=BookSerializer(book)
         return Response(ser.data,status=status.HTTP_200_OK)
     
+    def put (self,request,pk):
+        book=get_object_or_404(BookModel,id=pk)
+        ser=BookSerializer(book,data=request.data)
+        if ser.is_valid() :
+            ser.save()
+            return Response(ser.data,status=status.HTTP_200_OK)
+        return Response(ser.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def patch (self,request,pk):
+        book=get_object_or_404(BookModel,id=pk)
+        ser=BookSerializer(book,data=request.data,partial=True)
+        if ser.is_valid() :
+            ser.save()
+            return Response(ser.data,status=status.HTTP_200_OK)
+        return Response(ser.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        book=get_object_or_404(BookModel,id=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
